99e030f0aa80082a9203c36d751294b74e9a06f5
kuaifan <aipaw@live.cn>
2024-06-17 10:40:55 +0800

    no message

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,6 +9,23 @@ const config = {
   withCredentials: true,  // 异步请求携带cookie
 }
 
+const responseFormat = (data: any) => {
+  if (typeof data === "string") {
+    if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/.test(data)) {
+      data = utils.formatDate("Y-m-d H:i:s", new Date(data))
+    }
+  } else if (utils.isArray(data)) {
+    return data.map((item: any) => responseFormat(item))
+  } else if (utils.isJson(data)) {
+    for (const key in data) {
+      if (Object.prototype.hasOwnProperty.call(data, key)) {
+        data[key] = responseFormat(data[key])
+      }
+    }
+  }
+  return data
+}
+
 class RequestHttp {
   // 定义成员变量并指定类型
   service: AxiosInstance;
@@ -22,13 +39,12 @@ class RequestHttp {
      * 客户端发送请求 -> [请求拦截器] -> 服务器
      */
     this.service.interceptors.request.use(
-      function (config) {
+      config => {
         config.headers.Token = userState.getState().token
         return config
       },
-      function (error) {
+      error => {
         // 对请求错误做些什么
-        // console.log(error)
         return Promise.reject(error)
       }
     )
@@ -38,31 +54,32 @@ class RequestHttp {
      * 服务器换返回信息 -> [拦截统一处理] -> 客户端JS获取到信息
      */
     this.service.interceptors.response.use(
-      function (response) {
-        // 2xx 范围内的状态码都会触发该函数。
-        const dataAxios = response.data
-        //
-        if (!utils.isJson(dataAxios)) {
-          return Promise.reject({code: 422, msg: "Format error", data: dataAxios})
+      ({data}) => {
+        // 状态码 2xx 范围内的状态码都会触发该函数。
+        if (!utils.isJson(data)) {
+          // 格式错误
+          return Promise.reject({code: 422, msg: "Format error", data})
         }
-        if (dataAxios.code !== 200) {
-          if (dataAxios.code === 301 || dataAxios.code === 302) {
-            // 重定向
-            const params = {
-              code: dataAxios.code,
-              msg: encodeURIComponent(dataAxios.msg),
-            }
-            window.location.href = utils.urlAddParams(window.location.href, params)
-          } else if (dataAxios.code === 401) {
-            // 身份验证失败
-            userState.setState({user_id: 0})
+        if (data.code === 200) {
+          // 请求成功
+          return responseFormat(data)
+        }
+        if ([301, 302].includes(data.code)) {
+          // 重定向
+          const params = {
+            code: data.code,
+            msg: encodeURIComponent(data.msg),
           }
-          return Promise.reject(dataAxios)
+          window.location.href = utils.urlAddParams(window.location.href, params)
         }
-        return dataAxios
+        if (data.code === 401) {
+          // 身份验证失败
+          userState.setState({user_id: 0})
+        }
+        return Promise.reject(data)
       },
-      function (error) {
-        // 超出 2xx 范围的状态码都会触发该函数。
+      error => {
+        // 状态码超出 2xx 范围的状态码都会触发该函数。
         return Promise.reject({code: 421, msg: "Request failed", data: error})
       }
     )
