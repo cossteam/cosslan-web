3adb25c7ea2eef386c5b5d37dcd0c10466075dd6
kuaifan <aipaw@live.cn>
2024-07-01 23:55:06 +0800

    no message

--- src/api/interfaces/machine.ts
+++
@@ -1,6 +0,0 @@
-import http from "../index";
-import {Machine} from "@/api/types/machine.ts";
-
-export const machineCreateDevice = (params: Machine.CreateDeviceRequest) => {
-  return http.get<Machine.Info>('machine/create_device', params)
-};
